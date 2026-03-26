92db3912ec2e88a1595d013b313d4f376e04380d
aayush123aa <140230197+aayush123aa@users.noreply.github.com>
2026-03-26 14:32:08 +0530

    Refactor player scoring program structure

--- check.c
+++ check.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Union with all fields directly (no struct)
 union Player {
     char playername[50];
     char teamname[20];
@@ -10,25 +9,21 @@ union Player {
     float test3;
 };
 
-#define MAX_PLAYERS 10
 
 int main() {
-    // Since union shares memory, we declare separate arrays per field
-    char playernames[MAX_PLAYERS][50];
-    char teamnames[MAX_PLAYERS][20];
-    float test1[MAX_PLAYERS];
-    float test2[MAX_PLAYERS];
-    float test3[MAX_PLAYERS];
+	int i,t,count,c1,c2,c3,pc;
+	float sum,s1,s2,s3,ps;
+    char playernames[10][50];
+    char teams[3][20] = {"Red", "Blue", "Green"};
+    char teamnames[10][20];
+    float test1[10];
+    float test2[10];
+    float test3[10];
 
-    union Player p; // used as a temporary input buffer
-    int n;
+    union Player p; 
 
-    printf("Enter number of players (max %d): ", MAX_PLAYERS);
-    scanf("%d", &n);
-    if (n > MAX_PLAYERS) n = MAX_PLAYERS;
 
-    // Input
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < 10; i++) {
         printf("\nPlayer %d:\n", i + 1);
 
         printf("  Name: ");
@@ -52,22 +47,24 @@ int main() {
         test3[i] = p.test3;
     }
 
-    // Player-wise average
-    printf("\n--- Player-wise Averages ---\n");
-    for (int i = 0; i < n; i++) {
-        float sum = 0;
-        int count = 0;
+
+
+    printf("Player-wise Averages:\n");
+    for (i = 0; i < 10; i++) {
+		sum = 0;
+        count = 0;
         if (test1[i] >= 0) { sum += test1[i]; count++; }
         if (test2[i] >= 0) { sum += test2[i]; count++; }
         if (test3[i] >= 0) { sum += test3[i]; count++; }
         printf("%s: %.2f\n", playernames[i], count ? sum / count : 0);
     }
 
-    // Test-wise average
-    printf("\n--- Test-wise Averages ---\n");
-    float s1=0, s2=0, s3=0;
-    int c1=0, c2=0, c3=0;
-    for (int i = 0; i < n; i++) {
+
+
+    printf("Test-wise Averages:\n");
+    s1=0, s2=0, s3=0;
+    c1=0, c2=0, c3=0;
+    for ( i = 0; i < 10; i++) {
         if (test1[i] >= 0) { s1 += test1[i]; c1++; }
         if (test2[i] >= 0) { s2 += test2[i]; c2++; }
         if (test3[i] >= 0) { s3 += test3[i]; c3++; }
@@ -76,14 +73,14 @@ int main() {
     printf("Test2 Avg: %.2f\n", c2 ? s2/c2 : 0);
     printf("Test3 Avg: %.2f\n", c3 ? s3/c3 : 0);
 
-    // Team-wise average
-    printf("\n--- Team-wise Averages ---\n");
-    char teams[3][20] = {"Red", "Blue", "Green"};
-    for (int t = 0; t < 3; t++) {
-        float sum = 0; int count = 0;
-        for (int i = 0; i < n; i++) {
+
+    printf("\nTeam-wise Averages:\n");
+    
+    for (t = 0; t < 3; t++) {
+        sum = 0;count = 0;
+        for (i = 0; i < 10; i++) {
             if (strcmp(teamnames[i], teams[t]) == 0) {
-                float ps = 0; int pc = 0;
+                ps = 0; pc = 0;
                 if (test1[i] >= 0) { ps += test1[i]; pc++; }
                 if (test2[i] >= 0) { ps += test2[i]; pc++; }
                 if (test3[i] >= 0) { ps += test3[i]; pc++; }
